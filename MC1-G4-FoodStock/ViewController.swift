4cdce19972303db5161f2f899e3460ffaf3272fd
melinadewi <hds.melina@gmail.com>
2020-04-03 17:02:23 +0700

    Initial Commit

---
+++ MC1-G4-FoodStock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC1-G4-FoodStock
+//
+//  Created by Melina Dewi on 03/04/20.
+//  Copyright © 2020 Melina Dewi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
