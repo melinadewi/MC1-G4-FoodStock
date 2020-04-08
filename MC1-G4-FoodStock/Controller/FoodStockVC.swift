9f81c8451715265417a4e73b62e36a57029b584b
vctxr <victorsamuelcuaca131@gmail.com>
2020-04-08 18:09:07 +0700

    add search bar placeholder

--- MC1-G4-FoodStock/Controller/FoodStockVC.swift
+++ MC1-G4-FoodStock/Controller/FoodStockVC.swift
@@ -14,7 +14,8 @@ class FoodStockVC: UIViewController {
     
     let foodCell = "FoodCell"   // cell identifier
     
-    var listOfFoods: [FoodModel] = []
+    var listOfFoods: [FoodModel] = []       // food data
+    var filteredFoods: [FoodModel] = []     // this will hold the foods that the user searches for
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +26,13 @@ class FoodStockVC: UIViewController {
     }
     
     func setUpNavBar() {
-        // creates a search controller and add button
+        // create add button
         let addButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFood))
+        
+        // create search controller
         let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self    // this will inform this class of any text changes within the UISearchBar
+        searchController.searchBar.placeholder = "Search foods"
         
         // adding the search controller to the nav bar
         navigationItem.searchController = searchController
@@ -56,7 +61,30 @@ class FoodStockVC: UIViewController {
     
     @IBAction func didTapSort(_ sender: UIButton) {
         // do something when sort button is tapped
-        print("Sort button did tap")
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let dateEdited = UIAlertAction(title: "Default (Date Edited)", style: .default) { (action) in
+            // implement code
+            print("Sort by date edited")
+        }
+        
+        let lowestStock = UIAlertAction(title: "Lowest Stock", style: .default) { (action) in
+            // implement code
+            print("Sort by lowest stock")
+        }
+        
+        actionSheet.addAction(dateEdited)
+        actionSheet.addAction(lowestStock)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true, completion: nil)
+        
+        // silence the constraint alert warnings. possible bug in iOS(?)
+        actionSheet.view.subviews.flatMap({$0.constraints}).filter{ (one: NSLayoutConstraint) -> (Bool)  in
+           return (one.constant < 0) && (one.secondItem == nil) && (one.firstAttribute == .width)
+        }.first?.isActive = false
     }
     
     @objc func addFood() {
@@ -80,6 +108,13 @@ extension FoodStockVC: UITableViewDataSource, UITableViewDelegate {
         cell.foodModel = listOfFoods[indexPath.row]     // sets foodModel variable in FoodCell, this will trigger didSet in FoodCell
         
         return cell
+    }
+    
+    // did tap cell
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // do something
+        print("\(listOfFoods[indexPath.row].foodName) selected!")
     }
     
     // configure swipe action
@@ -109,5 +144,11 @@ extension FoodStockVC: UITableViewDataSource, UITableViewDelegate {
         }
         
         return 0
+    }
+}
+
+extension FoodStockVC: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // do something
     }
 }
