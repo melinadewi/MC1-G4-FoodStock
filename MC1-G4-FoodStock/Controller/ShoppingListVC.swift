324e9af6f8af689ede8bd31a4c202716d249aac8
melinadewi <hds.melina@gmail.com>
2020-04-09 14:02:17 +0700

    Add initial ViewController, cell, and storyboard

---
+++ MC1-G4-FoodStock/Controller/ShoppingListVC.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingListVC.swift
+//  MC1-G4-FoodStock
+//
+//  Created by Melina Dewi on 09/04/20.
+//  Copyright © 2020 Melina Dewi. All rights reserved.
+//
+
+import Foundation
---
+++ MC1-G4-FoodStock/Model/ShoppingModel.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingModel.swift
+//  MC1-G4-FoodStock
+//
+//  Created by Melina Dewi on 09/04/20.
+//  Copyright © 2020 Melina Dewi. All rights reserved.
+//
+
+import Foundation
