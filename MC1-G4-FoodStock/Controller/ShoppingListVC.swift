2b616148178fc168d9c16b11c7cbb2aebea7e09b
Diana Ambarwati Febriani <dianaambar745@gmail.com>
2020-04-09 14:32:19 +0700

    add shopping list

--- MC1-G4-FoodStock/Controller/ShoppingListVC.swift
+++ MC1-G4-FoodStock/Controller/ShoppingListVC.swift
@@ -2,8 +2,104 @@
 //  ShoppingListVC.swift
 //  MC1-G4-FoodStock
 //
-//  Created by Melina Dewi on 09/04/20.
+//  Created by Diana Ambarwati Febriani on 08/04/20.
 //  Copyright © 2020 Melina Dewi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ShoppingListVC: UITableViewController {
+
+    let identifier:String = "ShoppingCell"
+    
+    var listOfShopItems = ShoppingModel.createShopItem()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setUpNavBar()
+    }
+    
+    func setUpNavBar() {
+        // creates a search controller and add button
+        let addButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        
+        // adding the search controller to the nav bar
+        navigationItem.rightBarButtonItem = addButton
+    }
+
+    // MARK: - Table view data source
+}
+
+extension ShoppingListVC{
+    override func tableView( _ tableview: UITableView, numberOfRowsInSection section: Int) -> Int{
+    return listOfShopItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as? ShoppingCell {
+            cell.configureTheCell(shopItem: listOfShopItems[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
+
+ 
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//           let cell =  tableView.dequeueReusableCell(withIdentifier: "ShoppingCell", for: IndexPath)
+//
+//            let shop = listOfShopItems[indexPath.row]
+//
+//            cell.shopItemLabel?.text = "\(shop.shopItem)"
+//            return cell
+//        }
+        
+
+    
+    
+
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
--- MC1-G4-FoodStock/Model/ShoppingModel.swift
+++ MC1-G4-FoodStock/Model/ShoppingModel.swift
@@ -2,8 +2,21 @@
 //  ShoppingModel.swift
 //  MC1-G4-FoodStock
 //
-//  Created by Melina Dewi on 09/04/20.
+//  Created by Diana Ambarwati Febriani on 09/04/20.
 //  Copyright © 2020 Melina Dewi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct ShoppingModel{
+    var itemName: String
+}
+
+extension ShoppingModel {
+    static func createShopItem() -> [ShoppingModel]{
+        return [ShoppingModel(itemName: "Minyak Goreng"),
+            ShoppingModel(itemName: "Gula Pasir"),
+            ShoppingModel(itemName: "Kopi")
+        ]
+    }
+}
