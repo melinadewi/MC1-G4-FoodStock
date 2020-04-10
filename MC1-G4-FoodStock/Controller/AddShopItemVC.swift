105af682f6b400ca7691c9adb7d0af1bcfa151bc
Diana Ambarwati Febriani <dianaambar745@gmail.com>
2020-04-10 21:09:16 +0700

    add shop list items

---
+++ MC1-G4-FoodStock/Controller/AddShopItemVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddShopItemVC.swift
+//  MC1-G4-FoodStock
+//
+//  Created by Diana Ambarwati Febriani on 10/04/20.
+//  Copyright © 2020 Melina Dewi. All rights reserved.
+//
+
+import UIKit
+
+class AddShopItemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
